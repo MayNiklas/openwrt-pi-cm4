fadd58e21740efa7514c6765952cd4a9f8e5648b
MayNiklas <info@niklas-steffen.de>
2023-09-11 11:01:00 +0200

    use Nix in CI env

--- flake.nix
+++ flake.nix
@@ -1,31 +1,19 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    nix-environments.url = "github:nix-community/nix-environments";
   };
 
-  outputs = { nixpkgs, flake-utils, nix-environments, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-      in
-      rec {
-
-        # Use nixpkgs-fmt for `nix fmt'
-        formatter = pkgs.nixpkgs-fmt;
-
-        devShells = {
-
-          # `nix develop' will open a shell with the packages needed to work with OpenWrt
-          default = nix-environments.devShells.${system}.openwrt.overrideAttrs (old: {
-            extraPkgs = with pkgs; [ ];
-          });
-
-        };
-
-      }
-    );
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "i686-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    in
+    {
+      devShells = nixpkgs.lib.genAttrs systems
+        (system: rec {
+          default = openwrt.env;
+          # https://github.com/nix-community/nix-environments/blob/master/envs/openwrt/shell.nix
+          # for CI environments without .env
+          openwrt = import ./shell.nix { pkgs = import nixpkgs { inherit system; }; };
+        });
+    };
 }
