d614e5d8d4a7904baf24bc4308841abff8ebe5a3
MayNiklas <info@niklas-steffen.de>
2023-09-11 15:12:44 +0200

    use nix-environments

--- flake.nix
+++ flake.nix
@@ -1,19 +1,18 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nix-environments.url = "github:nix-community/nix-environments";
   };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, nix-environments }:
     let
       systems = [ "x86_64-linux" "i686-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
     in
     {
       devShells = nixpkgs.lib.genAttrs systems
         (system: rec {
-          default = openwrt.env;
-          # https://github.com/nix-community/nix-environments/blob/master/envs/openwrt/shell.nix
-          # for CI environments without .env
-          openwrt = import ./shell.nix { pkgs = import nixpkgs { inherit system; }; };
+          default = nix-environments.devShells.${system}.openwrt;
+          openwrt-ci = nix-environments.devShells.${system}.openwrt-ci;
         });
     };
 }
